19213a0f567611efaca189ee82fdde18992b43b2
Assel-Aljazwe <157559559+Assel-Aljazwe@users.noreply.github.com>
2024-03-17 23:16:53 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,12 +1,12 @@
-async function countMatches(array, key) {
+function countMatchesAsync(array, key) {
   return new Promise((resolve) => {
-    let count = 0;
-    array.forEach((item) => {
-      if (item === key) count++;
-    });
-    resolve(count);
+    setTimeout(() => {
+      const count = array.reduce((acc, item) => acc + (item === key ? 1 : 0), 0);
+      resolve(count);
+    }, 0); // Using setTimeout with 0 ms to simulate an asynchronous operation
   });
 }
+
 
 
 
