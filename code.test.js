6563887ed5c153c724880005d5930a7393f2b3ff
Assel-Aljazwe <157559559+Assel-Aljazwe@users.noreply.github.com>
2024-03-17 21:06:59 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,11 +1,12 @@
-const { countMatches } = require('./code');
+const { countMatchesAsync } = require('./code');
 
-describe('countMatches', () => {
-  test('correctly counts matches', async () => {
-    const array = [1, 2, 3, 2, 4, 2];
-    const key = 2;
-    const expectedCount = 3;
-    const count = await countMatches(array, key);
-    expect(count).toBe(expectedCount);
-  });
+test('correctly counts the number of matches', async () => {
+  const array = [1, 2, 3, 2, 4, 2];
+  const key = 2;
+  const expectedCount = 3;
+
+  const count = await countMatchesAsync(array, key);
+  expect(count).toBe(expectedCount);
 });
+
+
